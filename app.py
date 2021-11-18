83fd2b346b01033fd62a91d6c3fe787a71e7b87f
Bruno Brasil <70863338+brunobf09@users.noreply.github.com>
2021-11-18 09:06:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def aposta():
-    return """<head><center><h1>Recomendador de Apostas</h1></head></center>
+    return """<head><center><h1>Recomendador de Apostas</h1></head></center>"""
     
 if __name__ == '__main__':
     app.run(debug='False', host='0.0.0.0')

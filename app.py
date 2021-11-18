22b8eebf83a72aa327178cfebbb80173b11d4fa2
Bruno Brasil <70863338+brunobf09@users.noreply.github.com>
2021-11-18 09:01:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def aposta():
     return """<head><center><h1>Recomendador de Apostas</h1></head></center>
     
 if __name__ == '__main__':
-    app.run()
+    app.run(debug='False', host='0.0.0.0')

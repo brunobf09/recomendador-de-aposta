48084bbbcdc5f10f63bb1a1aa9eff082a426f088
Bruno Brasil <70863338+brunobf09@users.noreply.github.com>
2021-11-18 08:17:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def aposta():
-    html = bet()
     return """<head><center><h1>Recomendador de Apostas</h1></head></center>
     
 if __name__ == '__main__':

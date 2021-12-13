73f01681e30aa5874f385fd6e0c407ac9a1cb734
Bruno Brasil <70863338+brunobf09@users.noreply.github.com>
2021-12-13 16:27:25 -0400

    Create surebet.py

---
+++ surebet.py
@@ -0,0 +1,54 @@
+# importando biblioteca
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from time import sleep
+import pandas as pd
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver =webdriver.Chrome('chromedriver', chrome_options=chrome_options)
+
+league = open('league','r').readline().split(',')
+
+link = "https://www.betfair.com/exchange/plus/pt/futebol/"
+
+driver.get(link)
+
+while (len(driver.find_elements(By.ID,"onetrust-accept-btn-handler"))) < 1:
+    sleep(0.5)
+
+accept = driver.find_element(By.ID,"onetrust-accept-btn-handler")
+accept.click()
+sleep(1)
+
+teams = []
+under_over = []
+odds_events = []
+
+for l in league:
+    driver.get(link + l)
+    while (len(driver.find_elements(By.CLASS_NAME, 'bet-button-price'))) < 1:
+        sleep(0.5)
+
+    for odds in driver.find_elements(By.CLASS_NAME, 'bet-button-price'):
+        odds_events.append(odds.text)
+    for team in driver.find_elements(By.CLASS_NAME, 'name'):
+        teams.append(team.text)
+
+driver.quit()
+#
+odd = odds_events[::6]
+home = teams[::2]
+away = teams[1::2]
+#
+#
+betfair = {'HomeTeam':home,
+           'AwayTeam':away,
+           'Odd_Betfair':odd}
+#
+#
+betfair_df = pd.DataFrame(betfair)
+#
+betfair_df.to_json('betfair')

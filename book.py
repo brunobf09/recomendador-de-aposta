926b050e7ece4bc66de4fa8c0e2869c95592a1e0
Bruno Brasil <70863338+brunobf09@users.noreply.github.com>
2022-01-18 19:49:18 -0400

    Update book.py

--- book.py
+++ book.py
@@ -49,37 +49,37 @@ def predict(jogos,modelo):
 
   return y_pred , na
 
-def predict2(jogos):
-  jogos.dropna(inplace=True, axis=0)
-  jogos.reset_index(inplace=True,drop=True)
-  jogos["League"] = jogos.HomeTeam.apply(lambda x: list(book[book.HomeTeam == x].Div)[-1])
-  jogos['h5']  = jogos.AwayTeam.apply(lambda x: data[data.AwayTeam == x].FTHG.mean()**2)
-  jogos['a3']  = jogos.AwayTeam.apply(lambda x: data[data.AwayTeam == x].FTAG.mean()**2)
-  jogos['a5']  = jogos.HomeTeam.apply(lambda x: data[data.HomeTeam == x].FTAG.mean()**2)
-  jogos['h4']  = jogos.HomeTeam.apply(lambda x: data[data.HomeTeam == x].FTHG.mean()) * jogos.AwayTeam.apply(lambda x: data[data.AwayTeam == x].FTHG.mean())
+# def predict2(jogos):
+#   jogos.dropna(inplace=True, axis=0)
+#   jogos.reset_index(inplace=True,drop=True)
+#   jogos["League"] = jogos.HomeTeam.apply(lambda x: list(book[book.HomeTeam == x].Div)[-1])
+#   jogos['h5']  = jogos.AwayTeam.apply(lambda x: data[data.AwayTeam == x].FTHG.mean()**2)
+#   jogos['a3']  = jogos.AwayTeam.apply(lambda x: data[data.AwayTeam == x].FTAG.mean()**2)
+#   jogos['a5']  = jogos.HomeTeam.apply(lambda x: data[data.HomeTeam == x].FTAG.mean()**2)
+#   jogos['h4']  = jogos.HomeTeam.apply(lambda x: data[data.HomeTeam == x].FTHG.mean()) * jogos.AwayTeam.apply(lambda x: data[data.AwayTeam == x].FTHG.mean())
 
-  bias = json.load(open('bias.json'))
-  m5 = []
-  for h,a in zip(jogos.HomeTeam,jogos.AwayTeam):
-    if h not in bias:
-      x = 1
-    else:
-      x = bias[h]
-    if a not in bias:
-      y = 1
-    else:
-      y = bias[a]
-    m5.append((x+y)/2)
+#   bias = json.load(open('bias.json'))
+#   m5 = []
+#   for h,a in zip(jogos.HomeTeam,jogos.AwayTeam):
+#     if h not in bias:
+#       x = 1
+#     else:
+#       x = bias[h]
+#     if a not in bias:
+#       y = 1
+#     else:
+#       y = bias[a]
+#     m5.append((x+y)/2)
 
-  jogos['m5'] = m5 * jogos.h4
-  X = jogos[['m5','h5','a3','a5']]
-  std = StandardScaler()
-  X = std.fit_transform(X)
-  X = csr_matrix(X)
-  league = list(jogos.League)
-  y_pred = []
-  for x, l in zip(X, league):
-    model = joblib.load(f"model_{l}.pkl.z")
-    y_pred.append(model.predict(x))
+#   jogos['m5'] = m5 * jogos.h4
+#   X = jogos[['m5','h5','a3','a5']]
+#   std = StandardScaler()
+#   X = std.fit_transform(X)
+#   X = csr_matrix(X)
+#   league = list(jogos.League)
+#   y_pred = []
+#   for x, l in zip(X, league):
+#     model = joblib.load(f"model_{l}.pkl.z")
+#     y_pred.append(model.predict(x))
 
-  return y_pred
+#   return y_pred
